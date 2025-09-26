ac28dd9f797403046d1dd6e6104f1788b323d15c
JuliStrom <jstrom312@gmail.com>
2025-09-27 00:37:21 +0300

    practice in decomposition pagination

--- src/common/components/Pagination/Pagination.tsx
+++ src/common/components/Pagination/Pagination.tsx
@@ -1,5 +1,7 @@
 import s from './Pagination.module.css'
-import {getPaginationPages} from "@/common/utils/getPaginationPages.ts";
+import {PaginationControls} from "@/common/components/Pagination/PaginationControls/PaginationControls.tsx";
+import {PageSizeSelector} from "@/common/components/Pagination/PageSizeSelector/PageSizeSelector.tsx";
+
 
 type Props = {
     currentPage: number
@@ -12,42 +14,15 @@ type Props = {
 export const Pagination = ({currentPage, setCurrentPage, pagesCount, pageSize, changePageSize}: Props) => {
     if (pagesCount <= 1) return null
 
-    const pages = getPaginationPages(currentPage, pagesCount)
-
     return (
         <div className={s.container}>
-            <div className={s.pagination}>
-                {pages.map((page, idx) =>
-                        page === '...' ? (
-                            <span className={s.ellipsis} key={`ellipsis-${idx}`}>
-            ...
-          </span>
-                        ) : (
-                            <button
-                                key={page}
-                                className={
-                                    page === currentPage ? `${s.pageButton} ${s.pageButtonActive}` : s.pageButton
-                                }
-                                onClick={() => page !== currentPage && setCurrentPage(Number(page))}
-                                disabled={page === currentPage}
-                                type="button"
-                            >
-                                {page}
-                            </button>
-                        )
-                )}
-            </div>
-            <label>
-                Show
-                <select value={pageSize} onChange={e => changePageSize(Number(e.target.value))}>
-                    {[2, 4, 8, 16, 32].map(size => (
-                        <option value={size} key={size}>
-                            {size}
-                        </option>
-                    ))}
-                </select>
-                per page
-            </label>
+            <PaginationControls
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                pagesCount={pagesCount}/>
+            <PageSizeSelector
+                pageSize={pageSize}
+                changePageSize={changePageSize}/>
         </div>
     )
 }
