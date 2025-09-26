59bd2792f8becdcce04261e112f35734a6d4801f
JuliStrom <jstrom312@gmail.com>
2025-09-26 12:47:52 +0300

    done Pagination

---
+++ src/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistCover/PlaylistCover.tsx
@@ -0,0 +1,48 @@
+import s from "@/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistCover/PlaylistItem.module.css";
+import defaultCover from "@/assets/images/default-playlist-cover.png";
+import {useDeletePlaylistCoverMutation, useUploadPlaylistCoverMutation} from "@/features/playlists/api/playlistsApi.ts";
+import type {ChangeEvent} from "react";
+import type {Images} from "@/common/types";
+import {toast} from "react-toastify";
+
+type Props = {
+    playlistId: string
+    images: Images
+}
+
+export const PlaylistCover = ({playlistId, images}: Props) => {
+    const [uploadPlaylistCover] = useUploadPlaylistCoverMutation()
+    const [deleteCover] = useDeletePlaylistCoverMutation()
+
+
+
+    const uploadPlaylistCoverHandler =(event: ChangeEvent<HTMLInputElement>) => {
+        const maxSize = 1024 * 1024 // 1 MB
+        const allowedTypes = ['image/jpeg', 'image/png', 'image/gif']
+
+        const file = event.target.files?.length && event.target.files[0]
+        if(!file) return
+        if (!allowedTypes.includes(file.type)) {
+            toast('Only JPEG, PNG or GIF images are allowed', {type: 'error', theme: 'colored'})
+            return
+        }
+
+        if (file.size > maxSize) {
+            toast(`The file is too large. Max size is ${Math.round(maxSize / 1024)} KB, {type: 'error', theme: 'colored'}`)
+            return
+        }
+        uploadPlaylistCover({playlistId: playlistId, file})
+    }
+
+    const deleteCovetHandler = () => deleteCover({playlistId})
+
+    const originalCover = images.main.find(img=> img.type==='original')
+    const src = originalCover ? originalCover.url : defaultCover
+    return (
+        <>
+            <img src={src} alt="cover" width={'240px'} className={s.cover} />
+            <input type="file" accept={"image/jpeg, image/png, image/gif"} onChange={uploadPlaylistCoverHandler} />
+            {originalCover && <button onClick={deleteCovetHandler}>delete cover</button>}
+        </>
+    )
+}
--- src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
@@ -10,18 +10,19 @@ import {useState} from "react";
 import {PlaylistItem} from "@/features/playlists/ui/PlaylistsPage/PlaylistItem/PlaylistItem.tsx";
 import {EditPlaylistForm} from "@/features/playlists/ui/PlaylistsPage/EditPlaylistForm/EditPlaylistForm.tsx";
 import {useDebounceValue} from "@/common/hooks/useDebounceValue.ts";
+import {Pagination} from "@/common/components/Pagination/Pagination.tsx";
 
 
 export const PlaylistsPage = () => {
 
     const [search, setSearch] = useState('')
     const [playlistId, setPlaylistId] = useState<string | null>(null)
-
+    const [currentPage, setCurrentPage] = useState(1)
 
     const {register, handleSubmit, reset} = useForm<UpdatePlaylistArgs>()
 
     const debounceSearch = useDebounceValue(search)
-    const {data, isLoading} = useFetchPlaylistsQuery({search:debounceSearch})
+    const {data, isLoading} = useFetchPlaylistsQuery({search:debounceSearch, pageNumber: currentPage, pageSize: 5})
     const [deletePlaylist] = useDeletePlaylistMutation()
 
     console.log({search})
@@ -77,6 +78,11 @@ export const PlaylistsPage = () => {
                     )
                 })}
             </div>
+            <Pagination
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                pagesCount={data?.meta.pagesCount || 1}
+            />
         </div>
     )
 }
